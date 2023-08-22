bf98f9ab956edbdf3cfbd4f12afdeaeae51fdb44
Zegoris <zegor39@gmail.com>
2023-08-22 23:54:01 +0300

    Birthday

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', name='Айза')
+    return render_template('index.html', name='Нюта')
 
 @app.route('/text')
 def congratulation():
-    return render_template('text.html', name='Айза', text=text.split('\n'))
+    return render_template('text.html', name='Нюта', text=text.split('\n'))
 
 @app.route('/image')
 def image():
-    return render_template('img.html', name='Айза')
+    return render_template('img.html', name='Нюта')
 
 if __name__ == '__main__':
     app.run(port=7070, host='0.0.0.0')
